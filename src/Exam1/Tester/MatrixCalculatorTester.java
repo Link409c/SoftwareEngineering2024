fd38fe2548cc83e2f9670c789023a6ac0063483f
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-04-02 00:05:50 -0400

    tester class for calculator.

---
+++ src/Exam1/Tester/MatrixCalculatorTester.java
@@ -0,0 +1,20 @@
+package Exam1.Tester;
+
+/**
+ * Test Class for the Matrix Calculator Program.
+ */
+public class MatrixCalculatorTester {
+    /*
+     * Test Cases to detail:
+     * a. Incorrect Inputs to the methods should result in thrown exceptions.
+     *  1. Asymmetrical matrices should not pass the symmetry test.
+     *  2. Matrices of unequal size should cause sum and product methods to throw exceptions.
+     *  3. Matrices that are not square should cause trace method to throw an exception.
+     * b. Correct inputs should display good results as indicated by each method header.
+     *  1. This should hold true for matrices of any size, not just small ones.
+     * c. The toString method of the Matrix class should create a readable and well-formatted
+     *    display for the user to verify their outputs and the program functions.
+     * d. The Matrix class should alert the user when attempting to create an object which
+     *    has no rows or columns, and prompt them for the correct input.
+     */
+}
